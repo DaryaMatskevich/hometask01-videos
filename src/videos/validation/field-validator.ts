93ed2ac9ab618f3e21fd8f34cb968cf6ea6e426d
Darya Matskevich <darramatskevich@gmail.com>
2025-01-11 23:01:09 +0300

    changes

--- src/videos/validation/field-validator.ts
+++ src/videos/validation/field-validator.ts
@@ -1,21 +1,21 @@
 export const titleFieldValidator = (
     title: string | undefined,
-    errorsArray: Array<{ field: string; message: string }>) => {
+    errorsArray: Array<{ message: string; field: string }>) => {
     if (!title) {
-        errorsArray.push({ field: "title", message: "no title" })
+        errorsArray.push({ message: "no title", field: "title" })
     }
     if (title && title.trim().length > 40) {
-        errorsArray.push({ field: "title", message: "more than 40 symbols" })
+        errorsArray.push({ message: "more than 40 symbols", field: "title" })
 
     }
     if (title && title.trim().length < 1) {
-        errorsArray.push({ field: "title", message: "no title" })
+        errorsArray.push({ message: "no title", field: "title"})
 
     }
 }
 
 export const availableResolutionsFieldValidator = (availableResolutions: Array<string>,
-    errorsArray: Array<{ field: string; message: string }>) => {
+    errorsArray: Array<{ message: string; field: string }>) => {
     if (availableResolutions && availableResolutions.length) {
         availableResolutions.forEach((resolution: string) => {
             if (!Object.keys(ResolutionsEnum).includes(resolution)) {
@@ -31,16 +31,16 @@ export const availableResolutionsFieldValidator = (availableResolutions: Array<s
 
 export const authorFieldValidator = (
     author: string | undefined,
-    errorsArray: Array<{ field: string; message: string }>) => {
+    errorsArray: Array<{ message: string; field: string }>) => {
     if (!author) {
-        errorsArray.push({ field: "author", message: "no author" })
+        errorsArray.push({ message: "no author", field: "author"})
     }
     if (author && author.trim().length > 20) {
-        errorsArray.push({ field: "author", message: "more than 20 symbols" })
+        errorsArray.push({ message: "more than 20 symbols", field: "author" })
 
     }
     if (author && author.trim().length < 1) {
-        errorsArray.push({ field: "title", message: "no author" })
+        errorsArray.push({ message: "no author", field: "title" })
 
     }
 }
@@ -59,38 +59,38 @@ export enum ResolutionsEnum {
 
 export const validateCanBeDoWnlouded = (
     canBeDownloaded: boolean | undefined,
-    errorsArray: Array<{ field: string; message: string }>) => {
+    errorsArray: Array<{ message: string; field: string }>) => {
     if (canBeDownloaded === undefined) {
-        errorsArray.push({ field: "canBeDownloaded", message: "canBeDownloaded is not defined"})
+        errorsArray.push({ message: "canBeDownloaded is not defined", field: "canBeDownloaded" })
     }
     if (typeof canBeDownloaded !== "boolean") {
-        errorsArray.push({ field: "canBeDownloaded", message: "canBeDownloaded must be a boolean value"  })
+        errorsArray.push({ message: "canBeDownloaded must be a boolean value", field: "canBeDownloaded" })
 
     }
 }
 
 export const minAgeRestrictionValidator = (
     minAgeRestriction: number | undefined,
-    errorsArray: Array<{ field: string; message: string }>) => {
-    if (minAgeRestriction === undefined) {
-        errorsArray.push({ field: "minAgeRestriction", message: "minAgeRestriction is not defined"})
-    }
+    errorsArray: Array<{ message: string; field: string }>) => {
+    // if (minAgeRestriction === undefined) {
+    //     errorsArray.push({ message: "minAgeRestriction is not defined", field: "minAgeRestriction" })
+    // }
     if (typeof minAgeRestriction !== "number") {
-        errorsArray.push({ field: "minAgeRestriction", message: "minAgeRestriction must be a number value"  })
+        errorsArray.push({ message: "minAgeRestriction must be a number value" , field: "minAgeRestriction" })
     }
-    if (minAgeRestriction && minAgeRestriction < 18) {
-        errorsArray.push({ field: "minAgeRestriction", message: "minAgeRestriction must be 18 or more"})
-    }
+    // if (minAgeRestriction && minAgeRestriction < 18) {
+    //     errorsArray.push({ message: "minAgeRestriction must be 18 or more", field: "minAgeRestriction" })
+    // }
 }
 
 export const publicationDateValidator = (
     publicationDate: string | undefined,
-    errorsArray: Array<{ field: string; message: string }>) => {
+    errorsArray: Array<{ message: string; field: string }>) => {
     if (publicationDate === undefined) {
-        errorsArray.push({ field: "publicationDate", message: "publicationDate is not defined"})
+        errorsArray.push({ message: "publicationDate is not defined", field: "publicationDate" })
     } else {
        const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
         if (!dateRegex.test(publicationDate)) {
-            errorsArray.push({ field: "publicationDate", message: "publicationDate must be in the format 'YYYY-MM-DDTHH:MM:SS.MSZ'" });
+            errorsArray.push({ message: "publicationDate must be in the format 'YYYY-MM-DDTHH:MM:SS.MSZ'" , field: "publicationDate"});
         }
     }}
